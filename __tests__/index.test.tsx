d7455170e74f4a8a829a54548710e0024692b2e5
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2025-03-05 11:56:14 +0000

    changed the workflow

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -9,6 +9,33 @@ describe("Todo List", () => {
     cleanup(); // 🧑‍🏫 Clean up the DOM after each test
   });
 
+
+  //Write a test that asserts that loading is displayed when the response is not correct
+  it("should show loading when response is not correct", async () => {
+
+  });
+
+  // Write a test that asserts that a single item is in the list when the component is loadeed
+  it("should show todos when page is loaded", async () => {
+
+  });
+
+  // Write a test that asserts that a single item is in the list when the component is loaded
+  it("should show todos when page is loaded", async () => {
+
+  });
+
+  // Write a test that adds a new item to the list
+  it("should add a new todo", async () => {
+
+  });
+
+  // Write a test that removes an item from the list
+  it("should remove a todo", async () => {
+
+  });
+
+
   // 🧑‍🏫 Example test
   it("should show todos when page is loaded", async () => {
     render(<Home />);
