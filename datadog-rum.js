22a4a03cbcc6ebb7719164b1da66ed78691fb9b2
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2025-03-27 12:17:06 +0000

    datadog

--- datadog-rum.js
+++ datadog-rum.js
@@ -1,12 +1,12 @@
 import { datadogRum } from '@datadog/browser-rum';
 
 datadogRum.init({
-    applicationId: '98b79019-6d1b-4c29-b5e4-2059d2533e8d',
-    clientToken: 'pubd3ca3408176d0e76efe3f6c85ecab5e9',
+    applicationId: '80852c78-6366-4ae8-b655-2d636e95e803',
+    clientToken: 'pub98ebafdc8c58b006ca2201d502ceea4c',
     // `site` refers to the Datadog site parameter of your organization
     // see https://docs.datadoghq.com/getting_started/site/
     site: 'us5.datadoghq.com',
-    service: 'todo',
+    service: 'todo-app',
     env: 'staging',
     // Specify a version number to identify the deployed version of your application in Datadog
     // version: '1.0.0',
